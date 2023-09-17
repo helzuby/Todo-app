8ac15eddbdc43fcbd8f5974aed41889ab6c3d904
Nnachi Helen Chidinma <105408064+helzuby@users.noreply.github.com>
2023-09-17 21:44:30 +0200

    third commit

--- script.js
+++ script.js
@@ -3,58 +3,70 @@ const deletebutton = document.getElementById('delete');
 const input = document.getElementById('input-box');
 const ol = document.getElementById('list-container');
 
-
 let todoItems = getTodoFromLocalStorage();
 
 addbutton.addEventListener('click', addTodo);
-deletebutton.addEventListener('click', deleteTodo);
 
 
-//Function to add task to local storage
-function addTodo () {
-	const textInput = input.value.trim();
-	if (textInput === '') return;
+// Function to add task to local storage
+function addTodo() {
+  const textInput = input.value.trim();
+  if (textInput === '') return;
 
-	const todo = {
-		text: textInput,
-		id: Date.now()
-	};
+  const todo = {
+    text: textInput,
+    id: Date.now()
+  };
 
-	todoItems.push(todo);
-	saveTodoToLocalStorage(todoItems);
-	addTodoToList(todo); 
-	input.value = '';
+  todoItems.push(todo);
+  saveTodoToLocalStorage(todoItems);
+  addTodoToList(todo);
+  input.value = '';
 }
 
-//Function get task from local storage 
+// Function to get tasks from local storage
 function getTodoFromLocalStorage() {
-            const todo = JSON.parse(localStorage.getItem('todo'));
-            return todo || [];
-        }
- // function to add todo to HTML using JS
- function addTodoToList(todo) {
- 	const li = document.createElement('li');
- 	li.textContent = todo.text;
- 	ol.appendChild(li);
- }
+  const todo = JSON.parse(localStorage.getItem('todo'));
+  return todo || [];
+}
+
+// Function to add todo to HTML using JS
+function addTodoToList(todo) {
+  const li = document.createElement('li');
+  li.textContent = todo.text;
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.onclick = function () {
+    deleteTodoItem(todo.id);
+  };
+
+
+  ol.appendChild(li);
+   li.appendChild(deleteButton);
+}
 
 function saveTodoToLocalStorage(todo) {
-    localStorage.setItem('todo', JSON.stringify(todo));
+  localStorage.setItem('todo', JSON.stringify(todo));
 }
- 
- 	
- 	function deleteTodo() {
- 	
- 		if(todoItems.length > 0) {
- 			todoItems.pop();
- 			saveTodoToLocalStorage(todoItems);
-                renderTodo();
- 		}
- 	}
+
+function deleteTodoItem(id) {
+  todoItems = todoItems.filter((todo) => todo.id !== id);
+  saveTodoToLocalStorage(todoItems);
+  renderTodo();
+}
+
+function deleteTodo() {
+  if (todoItems.length > 0) {
+    todoItems.pop();
+    saveTodoToLocalStorage(todoItems);
+    renderTodo();
+  }
+}
 
 function renderTodo() {
-    ol.innerHTML = '';
-    todoItems.forEach(addTodoToList);
+  ol.innerHTML = '';
+  todoItems.forEach(addTodoToList);
 }
 
 renderTodo();
